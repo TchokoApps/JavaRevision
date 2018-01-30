9e0ad2eb23b15a1f8ca2415b691af7e430aaf07b
TchokoApps <35196627+TchokoApps@users.noreply.github.com>
2018-01-30 21:37:11 +0100

    Update Notes.cmd

--- Notes.cmd
+++ Notes.cmd
@@ -12,3 +12,53 @@ Apache Kafka links
 https://github.com/code-not-found/spring-kafka
 https://github.com/dsaish3/TeachApacheKafka/blob/master/Kafka_Commands.md
 https://github.com/dsaish3
+--------------------------------------------------------------
+https://github.com/gwenshap/kafka-examples
+https://github.com/code-not-found
+https://github.com/charithe/kafka-junit
+https://github.com/habren
+https://github.com/abhioncbr
+https://github.com/sdpatil
+https://github.com/hemin1003/spring-boot-study/tree/master/src/main/java/com/minbo/springdemo/web/kafka
+
+spring-boot-kafka
+https://github.com/john-deng/spring-boot-kafka
+https://github.com/bijukunjummen/sample-spring-kafka-producer-consumer
+
+https://github.com/nguacon90/spring-boot-kafka-integration (top)
+
+https://github.com/spring-projects/spring-boot/blob/master/spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/kafka/KafkaProperties.java
+
+https://github.com/howtoprogram (top)
+
+https://github.com/b0gg4rd/spring-kafka-example
+https://github.com/joshlong/spring-and-kafka
+https://github.com/SpringOnePlatform2016
+https://github.com/bijukunjummen/sample-spring-kafka-producer-consumer
+
+https://www.codenotfound.com/spring-kafka-boot-example.html
+https://github.com/wacai/spring-boot-starter-kafka
+http://javasampleapproach.com/java-integration/distributed-system/start-spring-apache-kafka-application-springboot
+
+http://javasampleapproach.com/ (mega top java page)
+http://www.baeldung.com/spring-kafka
+https://github.com/bijukunjummen/sample-spring-kafka-producer-consumer
+https://www.codenotfound.com/spring-kafka-consumer-producer-example.html
+https://github.com/eugenp/tutorials/tree/master/spring-kafka
+https://github.com/raphaelbrugier/spring-integration-kafka-sample/blob/master/src/main/java/com/github/rbrugier/spring/consumer/SoutKafkaListener.java
+
+file upload spring boot
+***************************
+https://github.com/bobbylight/file-upload-example/blob/master/src/main/java/org/fife/fileUpload/rest/UploadController.java
+search pattern: file upload spring boot site:https://github.com/
+
+https://github.com/spring-guides/gs-uploading-files
+
+https://github.com/hendisantika/spring-boot-file-upload-example
+https://github.com/netgloo/spring-boot-samples
+https://github.com/netgloo/spring-boot-samples/blob/master/spring-boot-file-upload-with-ajax/src/main/java/netgloo/controllers/MainController.java
+https://github.com/hendisantika/spring-boot-file-upload-example/blob/master/README.md
+https://github.com/hellokoding/uploadingfiles-springboot
+https://github.com/valor-software/ng2-file-upload/issues/875
+
+https://github.com/raphaelbrugier/spring-integration-kafka-sample/blob/master/src/main/java/com/github/rbrugier/spring/consumer/SoutKafkaListener.java
